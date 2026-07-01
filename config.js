b0bcf088f6bdc3672a39d5aaff6540629074c8f8
mdroot7282 <marcusd.root18@gmail.com>
2026-07-01 18:12:15 +0000

    Added config file

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    appTitle: "DHS Operations Dashboard",
+    agency: "Illinois State Police",
+    division: "Division of Internal Investigation",
+
+    map: {
+        center: [40.0, -89.2],
+        zoom: 7,
+        minZoom: 6,
+        maxZoom: 18
+    },
+
+    colors: {
+        green: "#2E7D32",
+        yellow: "#F9A825",
+        orange: "#EF6C00",
+        red: "#C62828",
+        ispBlue: "#003A70",
+        ispGold: "#FFD100"
+    }
+};
