eec007ed2d79c7651bf46ceff368a2f8e6a2f2e3
mdroot7282 <marcusd.root18@gmail.com>
2026-07-01 18:06:26 +0000

    Added DHS facility map

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
-const map=L.map('map').setView([40.0,-89.2],7);
+const map = L.map("map").setView([40.1,-89.2],7);
 
 L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 {
+attribution:"© OpenStreetMap",
 maxZoom:18
 }
 ).addTo(map);
 
+let totalAllegations=0;
+let totalOpened=0;
+let totalActive=0;
+let totalCharged=0;
+
 facilities.forEach(f=>{
 
-const marker=L.marker([f.lat,f.lng]).addTo(map);
+const marker=L.circleMarker(
+[f.lat,f.lng],
+{
+radius:8,
+color:"#003A70",
+fillColor:"#FFD100",
+fillOpacity:0.9,
+weight:2
+}
+).addTo(map);
 
-marker.bindPopup(`<b>${f.name}</b>`);
+marker.bindTooltip(
+`<b>${f.name}</b><br>
+Allegations: ${f.allegations}`,
+{
+direction:"top"
+}
+);
 
 marker.on("click",()=>{
 
 document.getElementById("details").innerHTML=`
-
 <h2>${f.name}</h2>
 
 <p><b>Allegations:</b> ${f.allegations}</p>
 
-<p><b>Opened:</b> ${f.opened}</p>
+<p><b>Cases Opened:</b> ${f.opened}</p>
 
-<p><b>Active:</b> ${f.active}</p>
+<p><b>Active Cases:</b> ${f.active}</p>
 
-<p><b>Charged:</b> ${f.charged}</p>
+<p><b>Cases Charged:</b> ${f.charged}</p>
 
-<p><b>Type:</b> ${f.type}</p>
-
+<p><b>Case Type:</b> ${f.type}</p>
 `;
 
 });
 
+totalAllegations+=Number(f.allegations||0);
+totalOpened+=Number(f.opened||0);
+totalActive+=Number(f.active||0);
+totalCharged+=Number(f.charged||0);
+
 });
 
 document.getElementById("facilityCount").innerText=facilities.length;
+document.getElementById("totalAllegations").innerText=totalAllegations;
+document.getElementById("totalOpened").innerText=totalOpened;
+document.getElementById("totalActive").innerText=totalActive;
+document.getElementById("totalCharged").innerText=totalCharged;
--- data.js
+++ data.js
@@ -1,12 +1,42 @@
 const facilities = [
 {
-    name:"Springfield DHS",
-    lat:39.7817,
-    lng:-89.6501,
-    allegations:0,
-    opened:0,
-    active:0,
-    charged:0,
-    type:""
+name:"Alton Mental Health Center",
+lat:38.9113430157224,
+lng:-90.1156514645056,
+allegations:0,
+opened:0,
+active:0,
+charged:0,
+type:""
+},
+{
+name:"Ann Kiley Developmental Center",
+lat:42.3461388308979,
+lng:-87.8488207933189,
+allegations:0,
+opened:0,
+active:0,
+charged:0,
+type:""
+},
+{
+name:"Chester Mental Health Center",
+lat:37.9321747291755,
+lng:-89.8557825761409,
+allegations:0,
+opened:0,
+active:0,
+charged:0,
+type:""
+},
+{
+name:"Chicago-Read Mental Health Center",
+lat:41.9554422480159,
+lng:-87.7985806766134,
+allegations:0,
+opened:0,
+active:0,
+charged:0,
+type:""
 }
 ];
